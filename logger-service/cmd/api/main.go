7329d0e7383ce4721fcf49bbff45b7959d11c562
Marina Rukavitsyna <marina.rukavitsyna@gmail.com>
2024-05-05 10:30:02 +0200

    update logger service

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"log-service/data"
+	"net/http"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+
+
 const (
-	webPort  = "80"
-	rpcPort  = "5001"
+	webPort = "80"
+	rpcPort = "5001"
 	mongoURL = "mongodb://mongo:27017"
 	gRpcPort = "50001"
 )
@@ -19,6 +24,7 @@ const (
 var client *mongo.Client
 
 type Config struct {
+	Models data.Models
 }
 
 func main() {
@@ -38,8 +44,27 @@ func main() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
 		}
-	}()
+	} ()
 
+	app := Config{
+		Models: data.New(client),
+	}
+
+	// start web server
+	go app.serve()
+
+}
+
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic()
+	}
 }
 
 func connectToMongo() (*mongo.Client, error) {
